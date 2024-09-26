22f8ea10929df75dd166a4ea4f3ae0bc0276ffaf
Mahlet Gebeyehu <mahlu.mek@gmail.com>
2024-09-25 21:15:21 -0400

    New added class

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -3,6 +3,16 @@ package com.bptn.course._05_strings;
 public class Strings {
 
 	public static void main(String[] args) {
+		String userName = "testuser";
+		System.out.println(userName.length());
+		System.out.println(userName.charAt(2));
+		System.out.println(userName.substring(4));
+		System.out.println(userName.substring(2,4));
+		System.out.println(userName.equals("testuser"));
+		System.out.println(userName.trim());//it ignores spaces 
+		System.out.println(userName.indexOf('r'));
+		
+		
 		
 	}
 }
